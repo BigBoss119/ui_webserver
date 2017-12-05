36af2342fa63d08406a11233385a3a3ae11f0cc2
Peter <1991pg@gmail.com>
2017-12-05 13:50:07 +0100

    autocomplete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser")
 const fs = require("fs")
+const cookieParser = require("cookie-parser")
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.set('view engine', 'pug')
@@ -35,30 +36,45 @@ app.post("/search", (request, response) => {
         if (err) {
             throw err;
         }
-        var allContent = request.body
-        console.log(request.body)
         var userList = JSON.parse(data)
+        var allContent = request.body.searchData
         var usersSug = []
+        
         for (var i = 0; i < userList.length; i++){
-            if (request.body.autocomplete === userList[i].firstname || request.body.autocomplete === userList[i].lastname || request.body.autocomplete === userList[i].email){
-                usersSug.push({
-                    firstname: userList[i].firstname,
-                    lastname: userList[i].lastname
-                })
+            if (allContent === userList[i].firstname.slice(0, allContent.length).toLowerCase() || allContent === userList[i].lastname.slice(0, allContent.length).toLowerCase() || allContent === userList[i].email.slice(0, allContent.length).toLowerCase()){
+                usersSug.push(userList[i].firstname + " " + userList[i].lastname
+                )
                 console.log("User found")
             }
         } 
         console.log("The suggestions:", usersSug)
-        response.status(200).send({search: usersSug})
+        response.json({status:200, search: usersSug})
+        // response.status(200).send({search: usersSug})
 });
-
 })
 
 
-app.get("/found", (request, response) => {
-    response.render("found", {title: "Found page",
-                                })
+app.post("/found", function (request, response) {
+    fs.readFile('users.json', function(err, data) {
+            if (err) {
+                throw err;
+            }
+            var theData = JSON.parse(data);
+            for (var i = 0; i < theData.length; i++) {
+                if (request.body.input === theData[i].firstname.toLowerCase() || request.body.input === theData[i].lastname.toLowerCase() || request.body.input === theData[i].email.toLowerCase() || request.body.input === theData[i].firstname.toLowerCase() + " " + theData[i].lastname.toLowerCase()) {   
+                    var firstName = theData[i].firstname;
+                    var secondName = theData[i].lastname;
+                    var emails = theData[i].email;
+                } console.log(firstName + secondName + emails)
+            } response.render("found", {  
+                firstname: firstName,
+                secondname: secondName,
+                email: emails,
+                })  
+
+ }) 
 })
+
 
 app.get("/add_user", (request, response) => {
     
@@ -87,3 +103,25 @@ response.redirect("/username")
 app.listen(3000, ()=>{
     console.log("Miracle happens on port 3000 =] ")
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
